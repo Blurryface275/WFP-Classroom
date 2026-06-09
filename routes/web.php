92a7ec5a8401ab23c804c827c863a8067cd35a27
Stephen <stephen.sugianto234@gmail.com>
2026-06-09 13:55:40 +0700

    feat: scaffold Laravel UI authentication scaffolding and assets

--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,7 @@ Route::resource('article', App\Http\Controllers\ArticleController::class);
 Route::get('/category/showExpensiveServices/{id}', [App\Http\Controllers\CategoryController::class, 'showExpensiveServices'])->name('category.showExpensiveServices');
 Route::post("/category/showInfo", [CategoryController::class, 'showInfo'])->name("category.showInfo");
 Route::post('/ajax/category/getEditForm',[CategoryController::class,'getEditForm'])->name('category.getEditForm');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
